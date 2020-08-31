614d56ded4399a08945b9b1fe5929643d62a83fd
emna <emna.sellamni@sqoin.us>
2020-08-31 12:03:06 +0100

    initial commit

---
+++ client/component/Card/CardExchanges.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import Card from './Card';
+
+const CardExchanges = () => (
+  <Card title="Exchanges">
+    {/* <a href="https://sqoin.exchange/" target="_blank" rel="nofollow noopener">Trade</a> */}
+  </Card>
+);
+
+export default CardExchanges;
---
+++ client/component/Card/CardLinks.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import Card from './Card';
+
+const CardLinks = () => (
+  <Card title="Links">
+    {/* <a href="https://sqoin.us/coin/" target="_blank" rel="nofollow noopener">Website</a><br />
+    <a href="https://www.facebook.com/sqoin/" target="_blank" rel="nofollow noopener">Facebook</a><br />
+    <a href="https://github.com/sqoin/" target="_blank" rel="nofollow noopener">Github</a><br />
+    <a href="https://www.linkedin.com/company/sqoin/" target="_blank" rel="nofollow noopener">Linkedin</a><br />
+    <a href="https://www.youtube.com/channel/UCQdDjelQcsXp32bpsupymBw?view_as=subscriber" target="_blank" rel="nofollow noopener">Youtube</a><br /> */}
+  </Card>
+);
+
+export default CardLinks;
